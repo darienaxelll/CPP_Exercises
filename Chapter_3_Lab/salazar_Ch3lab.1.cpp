b412e96dc3ea0a0ac16d19773de3ac838b2145a9
Darien Salazar <darien.axell.salazar@gmail.com>
2024-09-09 12:23:27 -0500

    Created chapter 3 lab

---
+++ Chapter_3_Lab/salazar_Ch3lab.1.cpp
@@ -0,0 +1,21 @@
+// Property Tax
+//
+// Madison County collects property taxes on the assessed value of property, which is 60 percent of its
+// actual value. For example, if a house is valued at $158,000, its assessed value is $94,800. This is the
+// amount the homeowner pays tax on. At last year’s tax rate of $2.64 for each $100 of assessed value,
+// the annual property tax for this house would be $2502.72. Write a program that asks the user to input
+// the actual value of a piece of property and the current tax rate for each $100 of assessed value. The
+// program should then calculate and report how much annual property tax the homeowner will be
+// charged for this property.
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello World." << endl;
+
+    return 0;
+}
